aefbdb1005221098f3b1edb94951423e2b43b13c
Sakthidaran Mariappa Prakash <sakthidaran.mprakash@gmail.com>
2024-06-30 00:28:10 +0200

    Added unit test for art fragment; removed bug in Viewmodel testing file

--- app/src/test/java/com/ArtBook/art/viewmodel/ArtViewModelTest.kt
+++ app/src/test/java/com/ArtBook/art/viewmodel/ArtViewModelTest.kt
@@ -1,14 +1,25 @@
 package com.ArtBook.art.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import com.ArtBook.art.getOrAwaitValueTest
 import com.ArtBook.art.repo.FakeArtRepository
+import com.ArtBook.art.roomdb.Art
 import com.ArtBook.art.util.Status
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.TestCoroutineScope
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 
 @ExperimentalCoroutinesApi
 class ArtViewModelTest {
@@ -16,17 +27,41 @@ class ArtViewModelTest {
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    /*
-    @get:Rule
-    var mainCoroutineRule = MainCoroutineRule()
+    private val testDispatcher = TestCoroutineDispatcher()
 
-     */
+    private val testCoroutineScope = TestCoroutineScope(testDispatcher)
 
     private lateinit var viewModel : ArtViewModel
+
+    @Mock
+    private lateinit var observer: Observer<List<Art>>
+
 
     @Before
     fun setup() {
         viewModel = ArtViewModel(FakeArtRepository())
+        MockitoAnnotations.openMocks(this)
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun cleanup() {
+        Dispatchers.resetMain() // Reset main dispatcher after testing
+        testCoroutineScope.cleanupTestCoroutines()
+    }
+
+    @Test
+    fun `deleteArt removes item from artList`() = testCoroutineScope.runBlockingTest {
+        val art = Art("Mona Lisa", "Da Vinci", 1503, "url")
+        viewModel.insertArt(art)
+        viewModel.deleteArt(art)
+
+        viewModel.artList.observeForever(observer)
+
+        // Assert and verify the observer's state
+        assert(viewModel.artList.value?.contains(art) == false)
+
+        viewModel.artList.removeObserver(observer)
     }
 
     @Test
@@ -56,4 +91,6 @@ class ArtViewModelTest {
 
         assertThat(value.status).isEqualTo(Status.ERROR)
     }
+
+
 }
